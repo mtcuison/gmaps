f2d0f0d03b08feee2ad1ad22d9d521b6597540af
mnv121 <97944690+mnv121@users.noreply.github.com>
2023-11-15 11:07:48 +0800

    refactor fetching of addresss into java
    problem on asynchronization on java to javascript to java fetching
    asynchronization of javascript does'nt refresh the value of address /marking's
    
    solution's switched to java then parse to json object that passed into javascript of map
    
    refactored initMap(coordinates, sAddressx, dTransact, isUserRoute) of index.php
    
    which executed in loadmap
    call by receiveJsonFromJava(jsonObject, isUserRoute)

--- src/org/rmj/gmaps/app/GMapsController.java
+++ src/org/rmj/gmaps/app/GMapsController.java
@@ -1,7 +1,14 @@
 package org.rmj.gmaps.app;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.concurrent.Worker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -11,6 +18,8 @@ import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.rmj.appdriver.GRider;
 import org.rmj.appdriver.SQLUtil;
 
@@ -30,18 +39,20 @@ public class GMapsController {
     private WebView sceneWeb;
     private WebEngine webengine;
     private boolean pbRoute = false;
-
+    private boolean pbreverseGeoCode = false;
+    
     private void toJSONArray() {
         String lsSQL = System.getProperty("app.sql.source");
-
+        String lsGeoCoded = "";
         try {
             ResultSet loRS = oApp.executeQuery(lsSQL);
             JSONArray jsonArray = new JSONArray();
 
-            while (loRS.next()) {
+            while (loRS.next()) {                
                 JSONObject jsonObject = new JSONObject();
-                jsonObject.put("dTransact", loRS.getString("dTransact"));
-
+                jsonObject.put("dTransact", loRS.getString("dTransact"));                
+                jsonObject.put("sAddressx",reverseGeoCode(loRS.getDouble("nLatitude"),loRS.getDouble("nLongitud")) );
+                //coordinates parsing
                 JSONArray nCoordinateArray = new JSONArray();
                 JSONArray coordinates = new JSONArray();
                 coordinates.add(loRS.getDouble("nLatitude"));
@@ -82,7 +93,8 @@ public class GMapsController {
             if (jsonObject.equals("[]")) System.exit(1);
                 webengine = sceneWeb.getEngine();
                 webengine.setJavaScriptEnabled(true);
-                            webengine.load("http://192.168.10.69/integsys/gMaps/index.php");
+                webengine.load("http://192.168.10.69/integsys/gMaps/index.php");
+                //webengine.load(System.getProperty("system.websvr") + "integsys/gMaps/index.php");
                 webengine.getLoadWorker().stateProperty().addListener((obs, oldValue, newValue) -> {
                     if (newValue == Worker.State.SUCCEEDED) {
                         webengine.executeScript("receiveJsonFromJava(" + jsonObject + ", "+ pbRoute +");");
@@ -95,6 +107,63 @@ public class GMapsController {
         }
     }
 
+  public static String reverseGeoCode(double lat, double lon){        
+        //API NI MAYNARD
+        String apiKey = "dFFTFYtJc0GyzeQBIh3VEjr4IamMhPos";
+        String apiUrl = "https://www.mapquestapi.com/geocoding/v1/reverse?key=" + apiKey +
+                "&location=" + lat + "%2C" + lon + "&outFormat=json&thumbMaps=false";
+
+            try {
+                URL url = new URL(apiUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                int responseCode = connection.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                        StringBuilder response = new StringBuilder();
+                        String inputLine;
+
+                            while ((inputLine = in.readLine()) != null) {
+                                response.append(inputLine);
+                            }
+
+                            in.close();
+
+                    
+                     JSONParser parser = new JSONParser();
+                     JSONObject jsonResponse = (JSONObject) parser.parse(response.toString());
+
+
+                // Extract street and city from the JSON response
+                JSONArray resultsArray = (JSONArray) jsonResponse.get("results");
+                if (resultsArray.size() > 0) {
+
+                // Extract street and city from the JSON response
+                JSONObject jsonResult = (JSONObject) resultsArray.get(0);
+                JSONArray locationsArray = (JSONArray) jsonResult.get("locations");
+                if (locationsArray.size() > 0) {
+                    JSONObject jsonLocation = (JSONObject) locationsArray.get(0);
+                    String street = (String) jsonLocation.get("street");
+                    String city = (String) jsonLocation.get("adminArea5"); 
+                    String locationInfo = street + ", " + city;
+                    
+//                    System.out.println ("Location :" +  locationInfo);
+                    return locationInfo;
+                    
+                }
+            }
+        }
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error fetching location information for (" + lat + ", " + lon + ")", e);
+        } catch (ParseException ex) {
+            Logger.getLogger(GMapsController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null; 
+    }
+  
     @FXML
     private void cmdCancel_Click(ActionEvent event) {
         unloadScene();
